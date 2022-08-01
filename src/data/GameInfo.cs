70e8f77a9e29eb6caebcdc977bee4623995b6ba1
P3NG00 <p3ng00minecraft@gmail.com>
2022-08-01 14:02:04 -0600

    minor code adjustent

--- src/data/GameInfo.cs
+++ src/data/GameInfo.cs
@@ -7,17 +7,15 @@ namespace Game.Data
     public static class GameInfo
     {
         private static float _tickDelta = 0f;
-        private static Vector2 _mouseBlock;
-        private static Point _mouseBlockInt;
         private static int[] _ticks = new [] {0, 0};
         private static int[] _lastTickDifferences = new int[10];
         private static float[] _lastFps = new float[10];
 
         public static Block CurrentBlock = Blocks.Dirt;
+        public static Vector2 LastMouseBlock { get; private set; }
+        public static Point LastMouseBlockInt { get; private set; }
 
         public static int Ticks => _ticks[0];
-        public static Vector2 LastMouseBlock => _mouseBlock;
-        public static Point LastMouseBlockInt => _mouseBlockInt;
         public static float AverageFramesPerSecond => _lastFps.Average();
         public static float AverageTicksPerFrame => (float)_lastTickDifferences.Average();
 
@@ -50,19 +48,19 @@ namespace Game.Data
             // get block position from mouse
             var mousePos = Input.MousePosition.ToVector2();
             mousePos.Y = Display.WindowSize.Y - mousePos.Y - 1;
-            _mouseBlock = ((mousePos - (Display.WindowSize.ToVector2() / 2f)) / Display.BlockScale) + (player.Position + new Vector2(0, player.Dimensions.Y / 2f));
-            _mouseBlockInt = _mouseBlock.ToPoint();
+            LastMouseBlock = ((mousePos - (Display.WindowSize.ToVector2() / 2f)) / Display.BlockScale) + (player.Position + new Vector2(0, player.Dimensions.Y / 2f));
+            LastMouseBlockInt = LastMouseBlock.ToPoint();
             // catch out of bounds
-            if (_mouseBlockInt.X >= 0 && _mouseBlockInt.X < world.Width &&
-                _mouseBlockInt.Y >= 0 && _mouseBlockInt.Y < world.Height)
+            if (LastMouseBlockInt.X >= 0 && LastMouseBlockInt.X < world.Width &&
+                LastMouseBlockInt.Y >= 0 && LastMouseBlockInt.Y < world.Height)
             {
                 bool ctrl = Input.KeyHeld(Keys.LeftControl) || Input.KeyHeld(Keys.RightControl);
                 if (ctrl ? Input.ButtonLeftFirstDown() : Input.ButtonLeftDown())
-                    world.Block(_mouseBlockInt) = Blocks.Air;
+                    world.Block(LastMouseBlockInt) = Blocks.Air;
                 else if (ctrl ? Input.ButtonRightFirstDown() : Input.ButtonRightDown())
-                    world.Block(_mouseBlockInt) = CurrentBlock;
+                    world.Block(LastMouseBlockInt) = CurrentBlock;
                 else if (ctrl ? Input.ButtonMiddleFirstDown() : Input.ButtonMiddleDown())
-                    world.Block(_mouseBlockInt).Update(_mouseBlockInt, world);
+                    world.Block(LastMouseBlockInt).Update(LastMouseBlockInt, world);
             }
         }
 
