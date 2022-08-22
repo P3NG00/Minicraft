486e61997e9731972ee87da0838a3f0af3259135
P3NG00 <p3ng00minecraft@gmail.com>
2022-08-21 20:33:37 -0600

    made readonly variable into constant

--- src/util/Display.cs
+++ src/util/Display.cs
@@ -10,6 +10,7 @@ namespace Minicraft.Utils
     public static class Display
     {
         public const int FRAMES_PER_SECOND = 60;
+        public const float FRAME_STEP = 1f / FRAMES_PER_SECOND;
         public const int BLOCK_SCALE_MIN = 5;
         public const int BLOCK_SCALE_MAX = 25;
 
@@ -17,8 +18,6 @@ namespace Minicraft.Utils
 
         public static SpriteBatch SpriteBatch { get; private set; }
         public static Point WindowSize => new Point(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
-
-        public static readonly float FrameStep = 1f / FRAMES_PER_SECOND;
 
         public static Vector2 CameraOffset;
         public static bool ShowGrid = false;
