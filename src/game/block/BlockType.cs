aabca1a24a06f803525fcc17a006cc3876bcf328
P3NG00 <p3ng00minecraft@gmail.com>
2022-08-23 13:53:00 -0600

    slightly adjusted grass block color

--- src/game/block/BlockType.cs
+++ src/game/block/BlockType.cs
@@ -24,7 +24,7 @@ namespace Minicraft.Game.Blocks
             s_blockArray = ImmutableArray.Create(new Block[] {
                 new Block("Air", new Color(240, 255, 255), true),
                 new Block("Dirt Block", new Color(96, 48, 0), texture: Textures.Shaded_BottomRight),
-                new GrassBlock("Grass Block", new Color(32, 128, 16), texture: Textures.Shaded_BottomRight),
+                new GrassBlock("Grass Block", new Color(32, 160, 16), texture: Textures.Shaded_BottomRight),
                 new Block("Stone Block", new Color(192, 192, 192), texture: Textures.Shaded_BottomRight),
                 new Block("Wood Block", new Color(128, 92, 32), true, Textures.Shaded_BottomRight),
                 new LeavesBlock("Leaves Block", new Color(48, 128, 32), true, Textures.Shaded_BottomRight),
