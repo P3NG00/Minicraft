9d71bdab4739cf79b10dee31e183f7c274490b46
P3NG00 <p3ng00minecraft@gmail.com>
2022-09-04 13:58:47 -0600

    TNT now explodes

--- src/game/block/TNTBlock.cs
+++ src/game/block/TNTBlock.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Minicraft.Game.Worlds;
 
 namespace Minicraft.Game.Blocks
 {
@@ -7,6 +8,21 @@ namespace Minicraft.Game.Blocks
     {
         public TNTBlock(string name, Color color, int hitsToBreak, bool canWalkThrough, Texture2D texture) : base(name, color, hitsToBreak, canWalkThrough, texture) {}
 
-        // TODO explode on break
+        public sealed override void Interact(World world, Point position)
+        {
+            // break blocks in a 5x5 area except for corners
+            for (int x = position.X - 2; x <= position.X + 2; x++)
+            {
+                for (int y = position.Y - 2; y <= position.Y + 2; y++)
+                {
+                    var isEdgeX = x == position.X - 2 || x == position.X + 2;
+                    var isEdgeY = y == position.Y - 2 || y == position.Y + 2;
+                    if (!isEdgeX || !isEdgeY)
+                        world.SetBlockType(x, y, BlockType.Air);
+                }
+            }
+            // base call
+            base.Interact(world, position);
+        }
     }
 }
