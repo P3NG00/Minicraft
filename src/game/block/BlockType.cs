3dca5f7b631b223fb674279f9ea0b610712a891f
P3NG00 <p3ng00minecraft@gmail.com>
2022-09-03 19:22:18 -0600

    removed strange spacing from BlockType.cs

--- src/game/block/BlockType.cs
+++ src/game/block/BlockType.cs
@@ -22,12 +22,12 @@ namespace Minicraft.Game.Blocks
         static Block()
         {
             s_blockArray = ImmutableArray.Create(new Block[] {
-                new Block      ("Air"         , new Color(240, 255, 255), 0, true , Textures.Blank ),
-                new Block      ("Dirt Block"  , new Color(96,   48,   0), 3, false, Textures.Shaded),
-                new GrassBlock ("Grass Block" , new Color(32,  160,  16), 3, false, Textures.Shaded),
-                new Block      ("Stone Block" , new Color(192, 192, 192), 7, false, Textures.Shaded),
-                new Block      ("Wood Block"  , new Color(128,  92,  32), 4, true , Textures.Shaded),
-                new LeavesBlock("Leaves Block", new Color(48,  128,  32), 2, true , Textures.Shaded),
+                new Block("Air", new Color(240, 255, 255), 0, true, Textures.Blank),
+                new Block("Dirt Block", new Color(96, 48, 0), 3, false, Textures.Shaded),
+                new GrassBlock("Grass Block", new Color(32, 160, 16), 3, false, Textures.Shaded),
+                new Block("Stone Block", new Color(192, 192, 192), 7, false, Textures.Shaded),
+                new Block("Wood Block", new Color(128, 92, 32), 4, true, Textures.Shaded),
+                new LeavesBlock("Leaves Block", new Color(48, 128, 32), 2, true, Textures.Shaded),
             });
         }
     }
