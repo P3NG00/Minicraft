2f9f3333069c0407e587c21294ab85a8d367aaab
P3NG00 <p3ng00minecraft@gmail.com>
2022-12-13 11:49:25 -0700

    extracted constants from function to class

--- src/scene/MainMenuScene.cs
+++ src/scene/MainMenuScene.cs
@@ -8,6 +8,11 @@ namespace MinicraftGame.Scenes
 {
     public sealed class MainMenuScene : AbstractSimpleScene
     {
+        // frequency of title rotation
+        private const float TITLE_ROT_FREQ = 0.025f;
+        // amplitude of title rotation
+        private const float TITLE_ROT_AMP = 0.125f;
+
         public MainMenuScene() : base()
         {
             var buttonWorldNew = new Button(new Vector2(0.5f, 0.6f), new Point(250, 50), "create world", Colors.ThemeBlue, CreateNewWorld);
@@ -21,9 +26,7 @@ namespace MinicraftGame.Scenes
         public sealed override void Draw()
         {
             // draw title
-            const float freq = 0.025f;
-            const float amp = 0.125f;
-            var rotation = MathF.Sin(Minicraft.Ticks * freq) * amp;
+            var rotation = MathF.Sin(Minicraft.Ticks * TITLE_ROT_FREQ) * TITLE_ROT_AMP;
             Display.DrawCenteredString(FontSize._48, new Vector2(0.5f, 0.35f), Minicraft.TITLE, Colors.UI_Title, rotation: rotation, drawStringFunc: Display.DrawStringWithShadow);
             // base call
             base.Draw();
