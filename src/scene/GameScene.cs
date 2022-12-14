f367e1b19e68ff56ca2dcb0cf9444673276cc617
P3NG00 <p3ng00minecraft@gmail.com>
2022-12-14 11:40:26 -0700

    made hotbar readonly and healthbar a getter

--- src/scene/GameScene.cs
+++ src/scene/GameScene.cs
@@ -24,14 +24,15 @@ namespace MinicraftGame.Scenes
         private const string TEXT_RESUME = "resume";
         private const float PLAYER_REACH_RADIUS = 5f;
 
+        private Vector2 HealthBarSize => new Vector2(150, 30);
+
         // readonly
-        private readonly Vector2 _barSize = new Vector2(150, 30);
+        private readonly GUIHotbar _hotbar = new GUIHotbar();
         private readonly Button _buttonRespawn;
         private readonly Button _buttonResume;
         private readonly Button _buttonMainMenu;
 
         // cache
-        private GUIHotbar _hotbar = new GUIHotbar();
         // TODO store tick count the time the hit happened. once certain amount of seconds/ticks have passed, nullify blockhit
         private Point _blockHitPos = new(-1);
         private int _blockHits = 0;
@@ -232,11 +233,11 @@ namespace MinicraftGame.Scenes
             // draw inventory hotbar
             _hotbar.Draw();
             // draw health bar
-            var drawPos = new Vector2((Display.WindowSize.X / 2f) - (_barSize.X / 2f), Display.WindowSize.Y - GUIHotbar.HotbarSize.Y - _barSize.Y);
-            Display.Draw(drawPos, _barSize, new(color: Colors.UI_Bar));
+            var drawPos = new Vector2((Display.WindowSize.X / 2f) - (HealthBarSize.X / 2f), Display.WindowSize.Y - GUIHotbar.HotbarSize.Y - HealthBarSize.Y);
+            Display.Draw(drawPos, HealthBarSize, new(color: Colors.UI_Bar));
             // adjust size to fit within bar
             drawPos += new Vector2(Util.UI_SPACER);
-            var healthSize = _barSize - new Vector2(Util.UI_SPACER * 2, Util.UI_SPACER);
+            var healthSize = HealthBarSize - new Vector2(Util.UI_SPACER * 2, Util.UI_SPACER);
             // readjust size to display real health
             healthSize.X *= Minicraft.Player.Life / Minicraft.Player.MaxLife;
             Display.Draw(drawPos, healthSize, new(color: Colors.UI_Life));
