ca2030f0db129f6f93dbecaea9499032875a797a
P3NG00 <p3ng00minecraft@gmail.com>
2022-12-18 20:02:07 -0700

    moved item comparison to parent GameObject class

--- src/game/objects/GameObject.cs
+++ src/game/objects/GameObject.cs
@@ -1,10 +1,11 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MinicraftGame.Utils;
 
 namespace MinicraftGame.Game.Objects
 {
-    public abstract class GameObject
+    public abstract class GameObject : IEquatable<GameObject>
     {
         public readonly int ID;
         public readonly string Name;
@@ -19,5 +20,33 @@ namespace MinicraftGame.Game.Objects
             Name = name;
             DrawData = drawData;
         }
+
+        public bool Equals(GameObject other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Name.Equals(other.Name) && DrawData.Equals(other.DrawData);
+        }
+
+        public sealed override bool Equals(object obj) => Equals(obj as GameObject);
+
+        public sealed override int GetHashCode() => Name.GetHashCode() ^ DrawData.GetHashCode();
+
+        public static bool operator ==(GameObject a, GameObject b)
+        {
+            bool aNull = ReferenceEquals(a, null);
+            bool bNull = ReferenceEquals(b, null);
+            if (aNull && bNull)
+                return true;
+            if (aNull || bNull)
+                return false;
+            if (ReferenceEquals(a, b))
+                return true;
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(GameObject a, GameObject b) => !(a == b);
     }
 }
--- src/game/objects/item/Item.cs
+++ src/game/objects/item/Item.cs
@@ -1,43 +1,14 @@
-using System;
 using Microsoft.Xna.Framework;
 using MinicraftGame.Game.Inventories;
 using MinicraftGame.Utils;
 
 namespace MinicraftGame.Game.Objects.ItemObject
 {
-    public class Item : GameObject, IEquatable<Item>
+    public class Item : GameObject
     {
         public Item(string name, DrawData drawData, int id) : base (name, drawData, id) {}
 
         // TODO override in future for items with special purposes
         public virtual void Use(Slot slot, Point blockPosition) => Minicraft.World.Interact(blockPosition);
-
-        public bool Equals(Item other)
-        {
-            if (ReferenceEquals(other, null))
-                return false;
-            if (ReferenceEquals(this, other))
-                return true;
-            return Name.Equals(other.Name) && DrawData.Equals(other.DrawData);
-        }
-
-        public sealed override bool Equals(object obj) => Equals(obj as Item);
-
-        public sealed override int GetHashCode() => Name.GetHashCode() ^ DrawData.GetHashCode();
-
-        public static bool operator ==(Item a, Item b)
-        {
-            bool aNull = ReferenceEquals(a, null);
-            bool bNull = ReferenceEquals(b, null);
-            if (aNull && bNull)
-                return true;
-            if (aNull || bNull)
-                return false;
-            if (ReferenceEquals(a, b))
-                return true;
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(Item a, Item b) => !(a == b);
     }
 }
